b2eb5acda0831fdba27397ac859f3ff6082b882f
MADAR15 <madarconsulting15@gmail.com>
2015-09-06 03:02:14 +0200

    update refresh script

--- Refresh-Nodes.sh
+++ Refresh-Nodes.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd /IoT-Modeler/nodes
-git pull origin master https://github.com/MADAR15/IoT-Modeler-Nodes.git
+git remote add origin  https://github.com/MADAR15/IoT-Modeler-Nodes.git
+git pull 
